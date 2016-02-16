7e7e8a3725352063845d9d90182a4af049c6ebf7
Brent Traut <brent@traut.com>
2016-02-15 23:33:11 -0800

    Added newline at EOF.

--- index.js
+++ index.js
@@ -4,7 +4,14 @@
 var EOL = require('os').EOL;
 
 function ChangeEOLCharLoader(content) {
+    // Replace the newlines.
     content = content.replace(/\r?\n/g, EOL);
+    
+    // Add newline at the end of file.
+    if (new RegExp(eol + '$').test(content)) {
+        str += EOL;
+    }
+    
     this.callback(null, content);
 }
 
